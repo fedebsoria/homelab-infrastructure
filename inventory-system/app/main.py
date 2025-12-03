2ec66201a64fbea22d57358ba3092e19bad4c25a
Federico B. Soria <fsoria7@outlook.com>
2025-12-03 12:15:42 +0100

    chore: add function that gives to the tables some data to test that everything works

--- inventory-system/app/main.py
+++ inventory-system/app/main.py
@@ -1,6 +1,7 @@
 import mysql.connector
 import time
 import sys
+from tabulate import tabulate
 
 # Connection config.
 # NOTE: 'host' is 'db', the name given to the service in docker-compose.yml
@@ -55,6 +56,52 @@ def create_tables(cursor):
 
     print("Creating table 'employees...")
     cursor.execute(table_employees)
+    
+def populate_data(conn, cursor):
+    """Inserts initial sample data."""
+
+    # 1. Check if data already exists to avoid duplicates
+    if cursor.fetchone()[0] > 0:
+        print("Data already exists. Skipping insertion.")
+        return
+    
+    print("Inserting sample data...")
+
+    # 2. Insert Material (The Laptop)
+    # Note: We use %s as place holders for security (prevents SQL Injection)
+    sql_material = "INSERT INTO material (name, category, description) VALUES (%s, %s, %s)"
+    val_material = ("Lenovo ThinkPad L460", "Laptop", "Lab Server with Docker")
+    cursor.execute(sql_material, val_material)
+
+    # 3. Capture the generated ID
+    laptop_id = cursor.lastrowid
+    print(f" -> Material inserted with ID: {laptop_id}")
+
+    # 4. Insert Employee (Assigning the Laptop ID)
+    sql_employee = "INSERT INTO employees (name, role, material_otorgado) VALUES (%s, %s, %s)"
+    val_employee = ("Admin Lab", "DevOps Student", laptop_id)
+    cursor.execute(sql_employee,val_employee)
+
+    # 5. COMMIT (Save changes permanently)
+    conn.commit()
+    print("Transaction committed")
+
+def fetch_data(cursor):
+    """Fetches and displays data using Tabulate"""
+    print("\n--- Current Inventory ---")
+
+    # Query with JOIN to show Employee Name + Laptop Name
+    query = """
+    SELECT e.id, e.name, e.role, m.name AS device
+    FROM employees e
+    LEFT JOIN material m ON e.given_material = m.id
+    """
+    cursor.execute(query)
+    result = cursor.fetchall()
+
+    # Print table
+    headers = ["ID", "Employee Name", "Role", "Assigned Device"]
+    print(tabulate(result, headers=headers, tablefmt="grid"))
 
 def main():
     conn = create_connection()
@@ -65,6 +112,8 @@ def main():
         cursor = conn.cursor()
         create_tables(cursor)
         print("\nSUCCESS: Database structure initialized correctly.")
+        populate_data(conn, cursor)
+        fetch_data(cursor) # Show the result
     except mysql.connector.Error as err:
         print(f"Error executing SQL: {err}")
     finally:
